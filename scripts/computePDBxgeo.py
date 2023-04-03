6121f531376d829df9278dc8c2b31a5e451d90a2
AMarinhoSN <ad45@sanger.ac.uk>
2023-04-03 21:53:36 +0100

    add new xgeo_flpath column to csv

--- scripts/computePDBxgeo.py
+++ scripts/computePDBxgeo.py
@@ -39,15 +39,19 @@ def compute_xgeo(kwargs):
         # if not, compute xgeo and write xgeo csv
         prefix = pdb_chain_flpth.split("/")[-1].split(".")[0]
         chain_output_dir = f"{output_dir}/{prefix[1:3]}"
-        os.makedirs(chain_output_dir, exist_ok=True)
-        entry = PDBx.entry(pdb_chain_flpth)
-        entry.xdata_df.to_csv(chain_output_dir+"/"+prefix+".csv")
+        # if xgeo csv exists, skip it
+        chain_xgeo_flpath = chain_output_dir+"/"+prefix+".csv"
+        if os.path.exists(chain_xgeo_flpath):
+            return None
+        # if not, compute xgeo and write data
+        else:
+            os.makedirs(chain_output_dir, exist_ok=True)
+            entry = PDBx.entry(pdb_chain_flpth)
+            entry.xdata_df.to_csv()
     # TODO investigate those erorrs
     except:
         print(f"Error: {pdb_chain_flpth}")
-    #except(AttributeError):
-    #    print(f"AttributeError: {pdb_chain_flpth}")
-
+    
 def addPDBChainsFlPaths(sampled_clstrd_pdb_df, mylocalpdb):
     '''
     add a new column with pdb chains path and excludes collumns
@@ -77,9 +81,9 @@ def addPDBChainsFlPaths(sampled_clstrd_pdb_df, mylocalpdb):
 
 mylocalpdb_path = "/home/antonio/Projects/HlxCnt/mypdb/"
 sampled_clstrd_path = "/home/antonio/Projects/HlxCnt/sampled_clustered_pdb.csv"
-output_dir = mylocalpdb_path+"/xgeo_chain/"
+xgeo_output_dir = mylocalpdb_path+"xgeo_chain/"
 ncpus = 4
-
+out_csv = "/home/antonio/Projects/HlxCnt/sampled_clustered_pdb_2.csv"
 # decompress chain gz files if needed
 decompressGZFiles(mylocalpdb_path)
 
@@ -88,17 +92,36 @@ decompressGZFiles(mylocalpdb_path)
 sampled_clstrd_pdb_df = pd.read_csv(sampled_clstrd_path, index_col=0)
 sampled_clstrd_pdb_df = addPDBChainsFlPaths(sampled_clstrd_pdb_df, mylocalpdb_path)
 
-# TODO: run melodia on each entry, store xgeo.csv files at mylocalpdb dir
-#       check if file already exist to avoid recomputing xgeo data!
+# run melodia on each entry, store xgeo.csv files at mylocalpdb dir
+# check if file already exist to avoid recomputing xgeo data!
+"""
 chain_fls = sampled_clstrd_pdb_df["chain_flpath"].values
 kwargs_to_proc = []
 for i in chain_fls:
-    kwargs_to_proc.append({"pdb_chain_flpth":i, "output_dir":output_dir})
+    kwargs_to_proc.append({"pdb_chain_flpth":i, "output_dir":xgeo_output_dir})
 
 pool = mp.Pool(ncpus)
 print(len(kwargs_to_proc))
 pool.map(compute_xgeo, kwargs_to_proc)
+"""
+# TODO: add xgeo file location column at sampled clustered
 
-# TODO: add xgeo file location colum at sampled clustered
+def get_all_files_in_dir(directory):
+    file_paths = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_paths.append(file_path)
+    return file_paths
+files_found = get_all_files_in_dir(xgeo_output_dir)
 
-# TODO: export write new columns on csv
+# add xgeo files paths at a new column
+paths_idx_dct = {}
+for f in files_found:
+    paths_idx_dct[f.split("/")[-1].split(".")[0]] = f
+sampled_clstrd_pdb_df["xgeo_chain_flpath"] = sampled_clstrd_pdb_df.index.map(paths_idx_dct)
+missing_xgeo_N = len(sampled_clstrd_pdb_df.loc[sampled_clstrd_pdb_df["xgeo_chain_flpath"].isna()])
+print(f"{missing_xgeo_N} entries which no xgeo data was computed")
+# update sample clustered pdb csv
+sampled_clstrd_pdb_df.to_csv(out_csv)
+print(":: DONE ::")
