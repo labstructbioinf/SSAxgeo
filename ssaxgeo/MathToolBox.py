f9767709858dcd54c71e028600c8a95b07e810ed
AMarinhoSN <ad45@sanger.ac.uk>
2023-07-14 18:52:36 +0100

    fix tabs

--- ssaxgeo/MathToolBox.py
+++ ssaxgeo/MathToolBox.py
@@ -43,51 +43,52 @@ def running_avg(x, N):
     return np.convolve(x, np.ones((N,))/N, mode='valid')
 
 def savitzky_golay(y, window_size, order, deriv=0, rate=1):
-	r"""
+    r"""
     Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-	The Savitzky-Golay filter removes high frequency noise from data.
-	It has the advantage of preserving the original shape and
-	features of the signal better than other types of filtering
-	approaches, such as moving averages techniques.
-	Parameters
-	----------
-	y : array_like, shape (N,)
-		the values of the time history of the signal.
-	window_size : int
-		the length of the window. Must be an odd integer number.
-	order : int
-		the order of the polynomial used in the filtering.
-		Must be less then `window_size` - 1.
-	deriv: int
-		the order of the derivative to compute (default = 0 means only smoothing)
-	Returns
-	-------
-	ys : ndarray, shape (N)
-		the smoothed signal (or it's n-th derivative).
-	"""
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    """
     # 1 - Input processing
-	try:
-		window_size = np.abs(int(window_size))
-		order = np.abs(int(order))
-	except(ValueError):#, msg):
-		raise ValueError("window_size and order have to be of type int")
-	if window_size % 2 != 1 or window_size < 1:
-		raise TypeError("window_size size must be a positive odd number")
-	if window_size < order + 2:
-		raise TypeError("window_size is too small for the polynomials order")
-	order_range = range(order+1)
-	half_window = (window_size -1) // 2
+    try:
+        window_size = np.abs(int(window_size))
+        order = np.abs(int(order))
+    except(ValueError):#, msg):
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
 
     # precompute coefficients
-	b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-	m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
 
     # pad the signal at the extremes with
-	# values taken from the signal itself
-	firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-	lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-	y = np.concatenate((firstvals, y, lastvals))
-	return np.convolve( m[::-1], y, mode='valid')
+    # values taken from the signal itself
+
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m[::-1], y, mode='valid')
 
 #-----| Stats functions |-------------------------------------------------------
 def is_normal_dist(x, sig_max=3):
